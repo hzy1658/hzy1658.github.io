046abd47219894c072da9a4bd2163c9b20874f04
hzy1658 <huangzy1658@163.com>
2024-12-01 15:27:26 +0800

    Site updated: 2024-12-01 15:27:25

---
+++ anzhiyu/random.js
@@ -0,0 +1,3 @@
+var posts=["2024/11/29/hello-world/","2024/11/29/AAAwatching/","2024/11/30/11-29-IZNA-KBS直拍数据/","2024/11/29/11-29 WayV KBS直拍数据/","2024/11/30/11-29-TWS-KBS直拍数据/","2024/12/01/11-29-TXT-KBS直拍数据/","2024/11/30/11-29 ENHYPEN KBS直拍数据/"];function toRandomPost(){
+    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
+  };
